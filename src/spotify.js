7edd34d2efaf994e9e250689a4672e21ea257700
JonathanNoriega <noriega.jonathan0815@gmail.com>
2024-01-20 01:18:38 -0500

    spotify.html working

--- src/spotify.js
+++ src/spotify.js
@@ -1,18 +1,31 @@
 //      *****  WORK IN PROGRESS  *****      \\
 
- function getSongs() {
-    fetch("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=JNoriega220&limit=10&api_key=36ed4e1fb9f4611b084d6e0ca19ef440&format=json")
-    .then( (response) => response.json())
-    .then((data)=> data)// output will be the required data
+fetch("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=JNoriega220&limit=10&api_key=36ed4e1fb9f4611b084d6e0ca19ef440&format=json")
+    .then( response => response.json())
+    .then(data => mostrarData(data))// output will be the required data
     .catch( (error) => console.log(error))
-    }
 
+const mostrarData = (data) => {
+    let body = ''
+    console.log(data)
+    let n_data = data['recenttracks']['track']
+    for(let i = 0; i < n_data.length; i++){
+        body+= `
+        <div class="song-card" style="display:flex; flex-direction: column; width: 128px; margin-left: 0.35vw; margin-right: 0.35vw;" >
+            <img class="song-album" src=${n_data[i]['image'][2]['#text']} height="128px" width="128px" alt="Album image">
+            </img>
+            <h2 class="song-title" style="font-size:1.3vw; flex-grow:1;">${n_data[i]['name']}</h2>
+        </div>
+        `
+        document.getElementById('songs').innerHTML = body
+    }    
+}
 
-let tracks = getSongs()
 // getSongs().then((data)=> console.log(data))
 
 
+// const showSongs = () => {
 
-console.log(tracks)
-for(i=0; i<5; i++){
-}
+// }
+
+
